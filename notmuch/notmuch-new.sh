b27057af40b801b11e75d6dcefb549d6d54cb17f
Igor Serebryany <igor47@moomers.org>
2021-05-23 23:16:43 -0500

    notmuch-new: better indexing
    we want the `inbox` tag to only be applied to messages in the inbox --
    we remove it from any messages in other folders. sometimes we apply
    other-folder tags to those.

--- notmuch/notmuch-new.sh
+++ notmuch/notmuch-new.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 notmuch new
-notmuch tag +spam -- folder:spam
+notmuch tag --batch << EOF
++spam -inbox -- folder:spam
++archive -inbox -- folder:archive
++sent -inbox -- folder:sent-mail
+-inbox -- tag:inbox NOT (path:cur OR path:new)
+EOF
