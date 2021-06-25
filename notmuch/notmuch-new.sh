f06e492bc950b82d78777edbdf87188675b793da
Igor Serebryany <igor47@moomers.org>
2021-06-25 01:29:31 -0500

    notmuch: also run mailograf

--- notmuch/notmuch-new.sh
+++ notmuch/notmuch-new.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+echo "looking for new mail to index..."
 notmuch new
+
+echo "updating tags..."
 notmuch tag --batch << EOF
 +spam -inbox -- folder:spam
 +archive -inbox -- folder:archive
 +sent -inbox -- folder:sent-mail
 -inbox -- tag:inbox NOT (path:cur OR path:new)
+-spam -- tag:spam NOT folder:spam
 EOF
+
+echo "reporting mail stats..."
+cd "/home/igor47/repos/mailograf"
+/home/igor47/.asdf/installs/python/3.8.9/bin/poetry run mailograf report
