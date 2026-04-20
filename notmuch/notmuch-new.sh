19b4582c03200a58472459474464c5c403478ca5
Igor Serebryany <igor47@moomers.org>
2026-04-20 01:11:45 +0000

    kill useless inbox tag

--- notmuch/notmuch-new.sh
+++ notmuch/notmuch-new.sh
@@ -8,7 +8,6 @@ notmuch tag --batch << EOF
 +spam -inbox -- folder:spam
 +archive -inbox -- folder:archive
 +sent -inbox -- folder:sent-mail
--inbox -- tag:inbox NOT (path:cur OR path:new)
 -spam -- tag:spam NOT folder:spam
 EOF
 
